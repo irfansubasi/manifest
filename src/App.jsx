fd1de1fdde2e788a9145e91fd449f1e9ab9e3a33
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-24 17:52:30 +0300

    add aos and cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/bundle';
 
 import { Autoplay, EffectFade } from 'swiper/modules';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import logo from './assets/manifestgold.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
   const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+    });
+  }, []);
 
   return (
     <>
@@ -68,10 +77,72 @@ function App() {
               </a>
             </li>
             <span> | </span>
-            <li>HEMEN ARA</li>
+            <li>
+              <a target="_blank" href="tel:05331451741"></a>HEMEN ARA
+            </li>
           </ul>
         </div>
       </header>
+      <main>
+        <section className="info-section container mx-auto">
+          <div className="flex flex-col md:flex-row items-center justify-between mt-56">
+            <div className="w-full md:w-1/2" data-aos="fade-left">
+              <img
+                src="./assets/images/entrance.webp"
+                alt="Sol Resim"
+                className="w-full h-auto rounded"
+              />
+            </div>
+
+            <div
+              className="w-full md:w-1/2 p-8 card-text"
+              data-aos="fade-right"
+            >
+              <p className="text-lg">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aperiam placeat autem perspiciatis molestiae molestias, beatae
+                reiciendis ea est nisi magnam.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Expedita natus odit voluptate error aperiam saepe.
+                <br />
+                <br />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas aliquam nulla, dolor dolorum magni perspiciatis iste
+                soluta voluptatibus tenetur nemo recusandae obcaecati neque nam
+                inventore.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col-reverse md:flex-row items-center justify-between mt-44">
+            <div className="w-full md:w-1/2 p-8 card-text" data-aos="fade-left">
+              <p className="text-lg">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aperiam placeat autem perspiciatis molestiae molestias, beatae
+                reiciendis ea est nisi magnam.
+                <br />
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Expedita natus odit voluptate error aperiam saepe.
+                <br />
+                <br />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptas aliquam nulla, dolor dolorum magni perspiciatis iste
+                soluta voluptatibus tenetur nemo recusandae obcaecati neque nam
+                inventore.
+              </p>
+            </div>
+            <div className="w-full md:w-1/2" data-aos="fade-right">
+              <img
+                src="./assets/images/food1.jpg"
+                alt="Sol Resim"
+                className="w-full h-auto rounded"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
